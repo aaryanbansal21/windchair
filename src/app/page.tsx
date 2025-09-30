eb5031c02365c71294b395fffd36c56d1abe1380
Aaryan Bansal <aban0969@uni.sydney.edu.au>
2025-09-30 22:35:57 +1000

    Temp fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-// src/app/page.tsx
+// src/app/_components/post.tsx
 
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <p className="text-2xl">Initial deployment setup for Windchair.</p>
-      </div>
-    </main>
-  );
+// This is a temporary, empty component to allow the initial deployment to pass.
+export function LatestPost() {
+  return null;
 }
